f114573fc3fa3807e3a908d55a4e5a20829ce4eb
Neo TRAN <neo_tran@outlook.com>
2020-06-09 00:47:59 -0500

    Se ha agregado la librería click para recibir distintas flags

---
+++ main.py
@@ -0,0 +1,26 @@
+import click
+from scripts import banner
+
+@click.command()
+@click.option("--scam"    , '-s', default="None" , 
+                help="Web template name.   \033[92mpython main.py -s Name\033[0m")
+@click.option("--password", '-p', default=False,  
+                help="View action history. \033[92mpython main.py -p True\033[0m")
+@click.option("--info", '-i', default=False,
+                help="View info tools.     \033[92mpython main.py -i True\033[0m")
+
+def main(scam, password, info):
+    if (scam != "None"):
+        print(f"Scam!! {scam}")
+    elif (password):
+        print("Holi")
+    elif (info):
+        banner.info()
+    else:
+        while (True):
+            option = banner.option()
+            if(option == None):
+                break
+
+if __name__ == "__main__":
+    main()
