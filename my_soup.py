de65ceb5c9f5fa013eb9d3a4923c6fb61e575c8d
Neo TRAN <neo_tran@outlook.com>
2020-06-10 01:16:52 -0500

    se ha agregado la librería subprocess para mandar a llamar otro script utilizando banderas

--- my_soup.py
+++ my_soup.py
@@ -1,26 +1,26 @@
 import click
-from scripts import banner
+import subprocess
+from scripts import props
 
 @click.command()
 @click.option("--scam"    , '-s', default="None" , 
                 help="Web template name.   \033[92mpython my_soup.py -s Name\033[0m")
 @click.option("--password", '-p', default=False,  
                 help="View action history. \033[92mpython my_soup.py -p True\033[0m")
-@click.option("--info", '-i', default=False,
+@click.option("--info",     '-i', default=False,
                 help="View info tools.     \033[92mpython my_soup.py -i True\033[0m")
 
 def main(scam, password, info):
     if (scam != "None"):
         print(f"Scam!! {scam}")
     elif (password):
-        print("Holi")
+        print("En construcción")
     elif (info):
-        banner.info()
+        props.info()
     else:
-        while (True):
-            option = banner.option()
-            if(option == None):
-                break
+        option = props.banner()
+        if(option != None and option != 'Help'):
+            subprocess.call(['python3', './scripts/server.py', '--scam', option])
 
 if __name__ == "__main__":
     main()
