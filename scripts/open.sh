744a8e8bb0fd59b43bcea0e6c2afd4956d822450
Neo TRAN <neo_tran@outlook.com>
2020-06-10 16:21:15 -0500

    El orden alctual de las terminales es tmux tilix xterm y konsole

--- scripts/open.sh
+++ scripts/open.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 { # try
-    konsole --noclose -e ssh -R 80:localhost:$2 $3.localhost.run &
-    konsole --noclose -e python3 ./scripts/server.py -s $1 -p $2
+    tmux new -s MySoup -n:dev "tmux new-window -n:server 'python3 ./scripts/server.py -s $1 -p $2' | tmux split 'ssh -R 80:localhost:$2 $3.localhost.run'"
 } || { # catch
     { 
-        xterm -e ssh -R 80:localhost:$2 $3.localhost.run &
-        xterm -e python3 ./scripts/server.py -s $1 -p $2
+        tilix -a session-add-down -e ssh -R 80:localhost:$2 $3.localhost.run |
+        tilix -a session-add-right -e python3 ./scripts/server.py -s $1 -p $2
     } || {
-        gnome-terminal -x ssh -R 80:localhost:$2 $3.localhost.run &
-        gnome-terminal -x python3 ./scripts/server.py -s $1 -p $2
+        {
+            xterm -e ssh -R 80:localhost:$2 $3.localhost.run &
+            xterm -e python3 ./scripts/server.py -s $1 -p $2
+        } || {
+            {
+                konsole --noclose -e ssh -R 80:localhost:$2 $3.localhost.run &
+                konsole --noclose -e python3 ./scripts/server.py -s $1 -p $2
+            } || {
+                printf "\033[0;38;5;9m\n\nCompatible terminal not found\033[0m\n\n"   
+            }
+        }
     }
 }
+
